1a0eb9f224fac7c1ef007581269e4583579a29cb
andycooxy <andycooxy@gmail.com>
2020-01-15 13:39:00 +0000

    [update] added home.dart page with basic views

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,4 @@
+import 'package:country_times/services/home.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(centerTitle: true,
-    title: Text("Country Applicaiton"),
-    backgroundColor: Colors.amber[800],),
-    body: Center(
-      child: Text("Here I am again.",
-        style: TextStyle(
-          color: Colors.red[300],
-          letterSpacing: 2.0,
-          fontFamily: "IndieFlower",
-          fontWeight: FontWeight.bold
-        ),
-    ),
-    ) ,
-    floatingActionButton: FloatingActionButton(
-      backgroundColor:  Colors.amber[300],
-      child: Center(
-        child:  Text("Add"),),
-    ),
-  ),
-));
+void main() => runApp(MaterialApp(home: Home()));
--- lib/services/home.dart
+++ lib/services/home.dart
@@ -1,10 +1,30 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-class Home extends StatefulWidget{
+class Home extends StatelessWidget{
   @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    return null;
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Country Time App"),
+        centerTitle: true,
+        backgroundColor: Colors.black,
+      ) ,
+      body: Center(
+        child: Text("Here we are lesone 7",
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontFamily: "IndieFlower",
+              color: Colors.purple,
+              letterSpacing: 2.3,
+              fontSize: 20.0
+        ),
+      ),),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){ print("you clicked me");},
+        child: Text("Add"),),
+    );
   }
 
 }
