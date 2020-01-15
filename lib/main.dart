46ea31a04212048215a48b89d3f20576e1f35427
andycooxy <andycooxy@gmail.com>
2020-01-15 13:13:09 +0000

    [update] completed lesson 6 with added font type feature

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,23 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
   home: Scaffold(
-    appBar: AppBar( title: Text("My Test application"),
-    centerTitle: true,
-  ),
-  body: Center(child: Text("This is my body"),),
-  floatingActionButton: FloatingActionButton(child: Text("Click Me"),)
+    appBar: AppBar(centerTitle: true,
+    title: Text("Country Applicaiton"),
+    backgroundColor: Colors.amber[800],),
+    body: Center(
+      child: Text("Here I am again.",
+        style: TextStyle(
+          color: Colors.red[300],
+          letterSpacing: 2.0,
+          fontFamily: "IndieFlower",
+          fontWeight: FontWeight.bold
+        ),
+    ),
+    ) ,
+    floatingActionButton: FloatingActionButton(
+      backgroundColor:  Colors.amber[300],
+      child: Center(
+        child:  Text("Add"),),
+    ),
   ),
 ));
-
