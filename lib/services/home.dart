67f8d7af2b16358f7947b96f579cd1df0dda1645
andycooxy <andycooxy@gmail.com>
2020-01-15 11:45:30 +0000

    [init] project startup

---
+++ lib/services/home.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/widgets.dart';
+
+class Home extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return null;
+  }
+
+}
+
